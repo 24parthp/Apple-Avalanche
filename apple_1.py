fe99111edb1470ac5a34f89e378ef4f99a987bd7
Parth Patel <24patepar@molineschools.org>
2024-02-22 13:04:24 -0600

    class

--- apple_1.py
+++ apple_1.py
@@ -1,6 +1,7 @@
 #   a123_apple_1.py
 import turtle as trtl
-import string
+import random
+import string 
 
 #-----setup-----
 apple_image = "Apple-Avalanche/apple.gif" # Store the file name of your shape
@@ -8,38 +9,26 @@ apple_image = "Apple-Avalanche/apple.gif" # Store the file name of your shape
 wn = trtl.Screen()
 wn.setup(width=1000, height=500)
 wn.addshape(apple_image) # Make the screen aware of the new file
-wn.bgpic('Apple-Avalanche/background.gif')
-wn.tracer(False)
+wn.bgpic("Apple-Avalanche/background.gif")
 
-apple = trtl.Turtle()
-appleYpos = apple.ycor()
-appleXpos = apple.xcor()
+class apple():
+  apple = trtl.Turtle()
 
-#-----functions-----
-# given a turtle, set that turtle to be shaped by the image file
-def draw_apple(active_apple):
-  global wn
+  def __init__(self):
+    self.xpos, self.ypos = random.randint(-200,220), 0
+    self.letter = random.choice(string.ascii_letters)
+  
+  def show_letter(self):
 
-  active_apple.goto(appleXpos, appleYpos)
-  active_apple.shape(apple_image)
-  keyToPress()
-  wn.update()
+  def draw_apple(active_apple, self):
+    active_apple.shape(apple_image)
+    self.show_letter()
+    wn.update()
 
-def keyToPress():
-  apple.write('A')
-
-def drop_apple():
-  global apple
-
-  apple.seth(270)
-  print(apple.ycor())
-  apple.forward(100)
-  print(apple.ycor())
-
-#-----function calls-----
-draw_apple(apple)
-
-wn.onkeypress(drop_apple, "a")
+  def move_turtle(self):
+    apple.seth(270)
+    apple.fd(100)
+    apple.clear()
 
 wn.listen()
 wn.mainloop()
